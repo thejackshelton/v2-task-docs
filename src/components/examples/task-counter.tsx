4dbc32195a0a3773a6bf721d232f786db872d78d
thejackshelton <me@jackshelton.com>
2025-02-03 10:43:43 -0600

    initial

---
+++ src/components/examples/task-counter.tsx
@@ -0,0 +1,19 @@
+import { component$, useSignal, useTask$ } from "@builder.io/qwik";
+
+export default component$(() => {
+    const text = useSignal('');
+    const count = useSignal(text.value.length);
+
+	useTask$(({ track }) => {
+		track(() => text.value);
+		count.value = text.value.length;
+	});
+
+	return (
+		<div>
+			<textarea bind:value={text} />
+			<button onClick$={() => (text.value = "")}>Clear</button>
+			<p>Count: {count.value}</p>
+		</div>
+	);
+});

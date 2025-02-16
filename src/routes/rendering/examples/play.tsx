684e4b3c21db4e90fc92118531f6ec465afb026b
thejackshelton <me@jackshelton.com>
2025-02-15 20:59:10 -0600

    parallelism

---
+++ src/routes/rendering/examples/play.tsx
@@ -0,0 +1,34 @@
+import { $, component$, useSignal, useTask$ } from "@qwik.dev/core";
+
+export default component$(() => {
+	const count = useSignal(0);
+	const dblCount = useSignal(1);
+
+	const someAsyncWork = $(async (param: number) => {
+		await new Promise((resolve) => setTimeout(resolve, 1000));
+		console.log("someAsyncWork", param);
+	});
+
+	useTask$(async ({ track }) => {
+		track(() => count.value);
+		await someAsyncWork(1);
+	});
+
+	useTask$(async ({ track }) => {
+		track(() => count.value);
+		await someAsyncWork(2);
+	});
+
+	useTask$(async ({ track }) => {
+		track(() => count.value);
+		await someAsyncWork(3);
+	});
+
+	return (
+		<div>
+			<button onClick$={() => (count.value += 1)}>
+				Count: {count.value}/{dblCount.value}
+			</button>
+		</div>
+	);
+});
